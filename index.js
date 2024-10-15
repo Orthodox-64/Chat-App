85a002fa005a486b083258507b7d9e3ba278907a
orthodox-64 <151670745+Orthodox-64@users.noreply.github.com>
2024-10-15 21:33:48 +0530

    done

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const app = express();
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on("connection", (socket) => {
+    socket.on('user-message',(message)=>{
+       io.emit("message",message);
+    });
+});
+
+server.listen(9000, () => {
+    console.log("server is running on port 9000");
+});
